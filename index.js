95a1c3043fc007bee668425b2d7874ac00d5ed79
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2020-09-23 11:16:27 -0300

    Add initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const paIterable = (a1, d) => {
+  // return an iterable to iterate trough an arithmetic progression based on the first element a1 and the diference d 
+  // an = a1 + ( n - 1 )*d , where "an" is the value of the nth term of the sequence
+}
+
+
+
+const schoolStudentsIterable = (schools) => {
+  // return an iterable to iterate through all students of all schools
+  // the schools is an array of school objects 
+  // school object has the following structure { students: ['name1', 'name2', ...] }  
+}
+
+export {
+  paIterable,
+  schoolStudentsIterable
+}
